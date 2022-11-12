67e48abd8c146824b40b45bf2532f606ecabdaf9
MauricioEin <mauricio.ein@gmail.com>
2022-11-12 19:45:36 +0200

    about-page

--- views/app-about.cmp.js
+++ views/app-about.cmp.js
@@ -1,15 +1,25 @@
 export default {
-	template: `
+    template: `
         <section class="about-page">
-            <div class="home-page-brand absolute">
-                <h1 class="slogan"> Work smart<br>not hard</h1>
-                <h1 class="appsus">AppSus</h1>
+            <div class="about-page-brand absolute">
+            <h1 class="appsus">AppSus</h1>
+
+                <h1 class="slogan"> Keep things <span>simple</span></h1>
             </div>
 
-            <div class="apps-logos absolute">
-                <h1 class="relative gmail"><router-link :to="'/mail'" ><iconify-icon inline icon="cib:gmail"></iconify-icon></router-link></h1>
-                <h1 class="relative keep"><router-link :to="'/keep'" ><iconify-icon inline icon="simple-icons:googlekeep"></iconify-icon></router-link></h1>
-                <h1 class="relative books"><router-link :to="'/book'" ><iconify-icon inline icon="ph:books-light"></iconify-icon></router-link></h1>
+            <div class="apps-logos flex">
+            <div class="app-info flex flex-column">
+                <div class="gmail"><router-link :to="'/maiml'" ><iconify-icon inline icon="cib:gmail"></iconify-icon></router-link></div>
+                <p class="">Mr. Mail: All your messages <span> easily managed in one place </span></p>
+            </div>
+            <div class="app-info flex flex-column">                
+                <div class="keep"><router-link :to="'/keep'" ><iconify-icon inline icon="simple-icons:googlekeep"></iconify-icon></router-link></div>
+                <p class="">Ms. Notes: Keep track of <span> all your great ideas, anytime </span></p>
+             </div>
+             <div class="app-info flex flex-column">               
+                <div class="books"><router-link :to="'/book'" ><iconify-icon inline icon="ph:books-light"></iconify-icon></router-link></div>
+                <p class="">Ms. Books: Have your <span> library organised and growing </span></p>
+                </div>
             </div>
         </section>
     `
